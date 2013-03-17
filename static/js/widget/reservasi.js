c16a043927c42cabb3a91634188c67f2e16c4f4a
her0satr <her0satr@yahoo.com>
2013-03-16 19:22:48 -0700

    Widget Config

--- static/js/widget/reservasi.js
+++ static/js/widget/reservasi.js
@@ -1,6 +1,6 @@
 function init_widget(param) {
 	var h = 635; var w = 500;
-	var url = 'http://lintasgps.com/rental/index.php/widget/reservasi/?company_id=' + escape(param.company_id) + '&widget_type=' + escape(param.widget_type);
+	var url = 'http://localhost/rental/trunk/index.php/widget/reservasi/?company_id=' + escape(param.company_id) + '&widget_type=' + escape(param.widget_type);
 	document.write("<iframe src='" + url + "' height='" + h + "' width='" + w + "' frameborder='0' scrolling='no'></iframe>");
 }
 
